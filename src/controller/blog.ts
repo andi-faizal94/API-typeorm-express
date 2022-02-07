ae73882c801d7315bcf7e23ec2b58102b10a54fe
Andi Faizal <andifaizal647@gmail.com>
2022-02-08 00:39:20 +0700

    add controller/blog.ts and split the service router

---
+++ src/controller/blog.ts
@@ -0,0 +1,68 @@
+import express, { Response, Request } from "express";
+import Blog from "../entities/Blog";
+
+export const createBlog = async (req: Request, res: Response) => {
+  try {
+    const { title_blog: titleBlog, content_blog: contentBlog } = req.body;
+    if (!titleBlog) {
+      throw new Error("Error titleBlog");
+    }
+    if (!contentBlog) {
+      throw new Error("Error contentBlog");
+    }
+
+    const blog = Blog.create({
+      title_blog: titleBlog,
+      content_blog: contentBlog,
+    });
+    await blog.save();
+    return res.json(blog);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+export const getBlog = async (req: Request, res: Response) => {
+  try {
+    const getBlog = await Blog.find();
+    return res.json(getBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+export const getBlogById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      throw new Error("it not id");
+    }
+    const getBlogById = await Blog.findOne(Number(id));
+    return res.json(getBlogById);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+export const updateBlog = async (req: Request, res: Response) => {
+  try {
+    const { title_blog: titleBlog, content_blog: contentBlog } = req.body;
+    const { id } = req.params;
+    await Blog.update(Number(id), {
+      title_blog: titleBlog,
+      content_blog: contentBlog,
+    });
+    const updateBlog = await Blog.find({
+      id: Number(id),
+    });
+    res.json(updateBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+export const deleteBlog = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const deleteBlog = await Blog.delete(Number(id));
+    return res.json(deleteBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
--- src/routes/Blog.ts
+++ src/routes/Blog.ts
@@ -1,79 +1,28 @@
 import express, { Response, Request } from "express";
-import Blog from "../entities/Blog";
+import {
+  createBlog,
+  getBlog,
+  getBlogById,
+  updateBlog,
+  deleteBlog,
+} from "../controller/blog";
 
 const router = express.Router();
-// create
-router.post("/blog", async (req: Request, res: Response) => {
-  try {
-    const { title_blog: titleBlog, content_blog: contentBlog } = req.body;
-    if (!titleBlog) {
-      throw new Error("Error titleBlog");
-    }
-    if (!contentBlog) {
-      throw new Error("Error contentBlog");
-    }
 
-    const blog = Blog.create({
-      title_blog: titleBlog,
-      content_blog: contentBlog,
-    });
-    await blog.save();
-    return res.json(blog);
-  } catch (error) {
-    throw new Error(error);
-  }
-});
+// create
+router.post("/blog", createBlog);
+
 // getAll
-router.get("/blog", async (req: Request, res: Response) => {
-  try {
-    const getBlog = await Blog.find();
-    return res.json(getBlog);
-  } catch (error) {
-    throw new Error(error);
-  }
-});
+router.get("/blog", getBlog);
+
 // getbyid
-router.get("/blog/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    if (!id) {
-      throw new Error("it not id");
-    }
-    const getBlogById = await Blog.findOne(Number(id));
-    return res.json(getBlogById);
-  } catch (error) {
-    throw new Error(error);
-  }
-});
+router.get("/blog/:id", getBlogById);
 
 // update
-router.put("/blog/:id", async (req: Request, res: Response) => {
-  try {
-    const { title_blog: titleBlog, content_blog: contentBlog } = req.body;
-    const { id } = req.params;
-    await Blog.update(Number(id), {
-      title_blog: titleBlog,
-      content_blog: contentBlog,
-    });
-    const updateBlog = await Blog.find({
-      id: Number(id),
-    });
-    res.json(updateBlog);
-  } catch (error) {
-    throw new Error(error);
-  }
-});
+router.put("/blog/:id", updateBlog);
 
 // delete
-router.delete("/blog/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const deleteBlog = await Blog.delete(Number(id));
-    return res.json(deleteBlog);
-  } catch (error) {
-    throw new Error(error);
-  }
-});
+router.delete("/blog/:id", deleteBlog);
 
 export {
   router as createBlog,
