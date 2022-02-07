7fc877324f1bec32a337775c135ab97f3ee30b22
Andi Faizal <andifaizal647@gmail.com>
2022-02-07 23:40:38 +0700

    refactor code in routes/Blog.js and change to destuctutring params, use cont than let, make consisten about Response, Request in function(res,req)

--- src/routes/Blog.ts
+++ src/routes/Blog.ts
@@ -24,34 +24,34 @@ router.post("/blog", async (req: Request, res: Response) => {
   }
 });
 // getAll
-router.get("/blog", async (req, res) => {
+router.get("/blog", async (req: Request, res: Response) => {
   try {
-    let getBlog = await Blog.find();
+    const getBlog = await Blog.find();
     return res.json(getBlog);
   } catch (error) {
     throw new Error(error);
   }
 });
 // getbyid
-router.get("/blog/:id", async (req, res) => {
+router.get("/blog/:id", async (req: Request, res: Response) => {
   try {
-    let id = req.params.id;
+    const { id } = req.params;
     if (!id) {
       throw new Error("it not id");
     }
-    let getBlog = await Blog.findOne(Number(id));
-    return res.json(getBlog);
+    const getBlogById = await Blog.findOne(Number(id));
+    return res.json(getBlogById);
   } catch (error) {
     throw new Error(error);
   }
 });
 
 // update
-router.put("/blog/:id", async (req, res) => {
+router.put("/blog/:id", async (req: Request, res: Response) => {
   try {
     const { title_blog: titleBlog, content_blog: contentBlog } = req.body;
-    const id = req.params.id;
-    const blog = await Blog.update(Number(id), {
+    const { id } = req.params;
+    await Blog.update(Number(id), {
       title_blog: titleBlog,
       content_blog: contentBlog,
     });
@@ -65,11 +65,11 @@ router.put("/blog/:id", async (req, res) => {
 });
 
 // delete
-router.delete("/blog/:id", async (req, res) => {
+router.delete("/blog/:id", async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    const deleted = await Blog.delete(Number(id));
-    return res.json(deleted);
+    const deleteBlog = await Blog.delete(Number(id));
+    return res.json(deleteBlog);
   } catch (error) {
     throw new Error(error);
   }
