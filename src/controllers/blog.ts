e8d97f829a32c1a2898eca98a36c54bae93c6dce
Andi Faizal <andifaizal647@gmail.com>
2022-02-09 00:57:51 +0700

    add base respon and next function in controllers

--- src/controllers/blog.ts
+++ src/controllers/blog.ts
@@ -1,7 +1,11 @@
-import { Response, Request } from "express";
+import { Response, Request, NextFunction } from "express";
 import Blog from "../entities/Blog";
 
-export const createBlog = async (req: Request, res: Response) => {
+export const createBlog = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
   try {
     const { title_blog: titleBlog, content_blog: contentBlog } = req.body;
     if (!titleBlog) {
@@ -16,35 +20,59 @@ export const createBlog = async (req: Request, res: Response) => {
       content_blog: contentBlog,
     });
     await blog.save();
-    return res.json(blog);
+    return res.status(201).json(blog);
   } catch (error) {
-    throw new Error(error);
+    next(error);
   }
 };
-export const getBlog = async (req: Request, res: Response) => {
+
+export const getBlog = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
   try {
     const getBlog = await Blog.find();
-    return res.json(getBlog);
+    return res.status(200).json(getBlog);
   } catch (error) {
-    throw new Error(error);
+    next(error);
   }
 };
-export const getBlogById = async (req: Request, res: Response) => {
+export const getBlogById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
   try {
     const { id } = req.params;
     if (!id) {
       throw new Error("it not id");
     }
     const getBlogById = await Blog.findOne(Number(id));
-    return res.json(getBlogById);
+    return res.status(200).json(getBlogById);
   } catch (error) {
-    throw new Error(error);
+    next(error);
   }
 };
-export const updateBlog = async (req: Request, res: Response) => {
+
+export const updateBlog = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
   try {
     const { title_blog: titleBlog, content_blog: contentBlog } = req.body;
     const { id } = req.params;
+    if (!id) {
+      throw new Error("error it's not id");
+    }
+    if (!titleBlog) {
+      throw new Error("error it's not content in title_blog");
+    }
+    if (!contentBlog) {
+      throw new Error("error it's not content in content_blog");
+    }
+
     await Blog.update(Number(id), {
       title_blog: titleBlog,
       content_blog: contentBlog,
@@ -52,17 +80,21 @@ export const updateBlog = async (req: Request, res: Response) => {
     const updateBlog = await Blog.find({
       id: Number(id),
     });
-    res.json(updateBlog);
+    res.status(200).json(updateBlog);
   } catch (error) {
-    throw new Error(error);
+    next(error);
   }
 };
-export const deleteBlog = async (req: Request, res: Response) => {
+export const deleteBlog = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
   try {
     const { id } = req.params;
     const deleteBlog = await Blog.delete(Number(id));
-    return res.json(deleteBlog);
+    return res.status(200).json(deleteBlog);
   } catch (error) {
-    throw new Error(error);
+    next(error);
   }
 };
